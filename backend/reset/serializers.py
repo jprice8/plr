8276706b0f9ccd66f55fce2fae96af5e14dcec9b
Jackson Price <jdprice1995@gmail.com>
2021-06-24 20:23:10 -0500

    Dashboard chart API working and tested.

--- backend/reset/serializers.py
+++ backend/reset/serializers.py
@@ -9,7 +9,7 @@ class ItemresetSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Itemreset
-        fields = ('id', 'par', 'user', 'reset_level', 'week')
+        fields = ('id', 'par', 'user', 'reset_level', 'last_updated', 'week')
 
 
 class ParSerializer(serializers.ModelSerializer):
